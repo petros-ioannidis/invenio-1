jalavik/invenio

BibAuthorID: interface and backend upgrade
Short story:

* Responsive interface layouts, major refactoring
  and technology update

* Rabbit upgrade:
  - Smart caching
  - Major optimizations
  - Test suite

Slightly longer story:

* Fixed broken links for claiming multiple papers
* Improved the context of the email sent when an rt ticket is created
* The deamon which precomputes caches is no longer taking account empty authors
* Fixed menu bar to work even when an author does not have a canonical name
* Fixed backend name utilities: splitting and capitalization
* Fixed internal publication list on author page
* Fixed merge profiles link handling
* Fixed person search engine issues
* Fixed profiles merging algorithm
* Introduces a new API add_cname_to_hepname_record()
  to automatically schedule a BibUpload bibtask to
  append a new person ID to a given record.
* Moves retry_mkstemp() from bibdocfile to shellutils
  so that other modules can use it.
* jinja2 templates
* Author search engine fixes and optimizations
* Improvements in job scheduler
* Indexer optimization, now invenio search engine can
  index canonical names efficiently
* Session is now correctly initialized before use.
* Fixes in the WAPAFF format, now affiliations are computed
  correctly, both repeatable and omonymns.
* Rabbit: pluggable support for arbitrary identifiers
  (only ORCID and INSPIRE-ID implemented so far)
* author/%s redirects to author/profile/%s in the standard case
* Better handling of author pages when bibauthorid is disabled
  (restores old behaviour, disables menus)
* Unified bibauthorid_enabled flag make it easier to switch
  bibauthorid on and off
* Regression tests for profile pages
* Improvements in logging facility
* Rabbit now use a pluggable function to transform strings
  before doing an exact string search
* BibAuthorID now displays the strings directly from records
  without applying any beautyfication function
* Fixed /author/profile/ name resolution bug
* Fixed import

modules/miscutil/lib/upgrades/invenio_2014_01_23_bibauthorid_rabbit_matchable_name_column.py
@@ -1,0 +1,69 @@
+# -*- coding: utf-8 -*-
+##
+## This file is part of Invenio.
+## Copyright (C) 2013 CERN.
+##
+## Invenio is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## Invenio is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+import warnings
+from invenio.dbquery import run_sql
+from invenio.bibauthorid_general_utils import update_status
+from invenio.bibauthorid_dbinterface import get_name_by_bibref, create_matchable_name
+from invenio.bibauthorid_rabbit import rabbit
+
+depends_on = ['invenio_2013_11_28_bibauthorid_search_engine_column_changes']
+
+def info():
+    return "Updates the columns of aidPERSONIDPAPERS, adds the rabbit matchable name, assigns default value to columns."
+
+def do_upgrade():
+    warnings.filterwarnings('ignore')
+    run_sql("alter table aidPERSONIDPAPERS add `m_name` VARCHAR(256) not null after name")
+
+    run_sql("alter table aidPERSONIDPAPERS add INDEX `m_name-b` (`m_name`)")
+
+    present_bibrefs = set(run_sql("select bibref_table, bibref_value from aidPERSONIDPAPERS"))
+
+    total_updates = len(present_bibrefs)
+
+
+    records_for_rabbit = set()
+    for i,bibref in enumerate(present_bibrefs):
+        update_status(float(i)/total_updates, '%s out of %s (%s)' % (str(i), str(total_updates), str(bibref)))
+        try:
+            name = get_name_by_bibref(bibref)
+        except AssertionError, error:
+            if "A bibref must have exactly one name" in error.message:
+                records_for_rabbit.add(bibref[1])
+            else:
+                raise error
+        else:
+            m_name = create_matchable_name(name)
+            run_sql("update aidPERSONIDPAPERS set name=%s, m_name=%s where bibref_table=%s "
+                    "and bibref_value=%s ", (name, m_name, bibref[0], bibref[1]))
+    if records_for_rabbit:
+        rabbit(records_for_rabbit)
+    update_status(1., 'Finished')
+
+    run_sql("alter table aidPERSONIDDATA modify  data varchar(256) not null default '' ")
+
+def estimate():
+    """
+    Let's assume 2ms per sql query in a standard production environment, with some safety margin.
+    """
+    n = run_sql("select count(*) from aidPERSONIDPAPERS")[0][0]
+    queries = n*2
+    return 0.002 * queries
+

petros-ioannidis/invenio-1

* New Author Profile Pages, featuring
  responsive interface layouts, major refactoring
  and technology update.
  - New names comparison scheme for
    better matching
* Tortoise upgrade:
  - Better preclustering algorithm
  - Improvements in bibsched usage
  - Merging algorithm improvements
* Search engine:
  - Improved indexing
  - Improved name matching
  - Fixes and optimizations
    in both search engine and
    fallback search
* Fixed broken links for claiming multiple papers.
* Improved the context of the email sent when an rt ticket is created.
* The deamon which precomputes caches is no longer taking account empty authors.
* Fixed menu bar to work even when an author does not have a canonical name.
* Fixed backend name utilities: splitting and capitalization.
* Fixed internal publication list on author page.
* Fixed merge profiles link handling.
* Fixed person search engine issues.
* Fixed profiles merging algorithm.
* jinja2 templates.
* Author search engine fixes and optimizations.
* Improvements in job scheduler.
  index canonical names efficiently.
  (only ORCID and INSPIRE-ID implemented so far).
* author/%s redirects to author/profile/%s in the standard case.
  (restores old behaviour, disables menus).
  bibauthorid on and off.
* Regression tests for profile pages.
* Improvements in logging facility.
  before doing an exact string search.
  without applying any beautyfication function.
* Fixed /author/profile/ name resolution bug.
* Fixed import of publications from orcid.
* Smart marc caching: now bunches of records can be cached on demand.
* Finished implementation of autoclaiming box functionalities.
* Ported interface do BootStrap3.
  Porting still in progress regarding claiming interface.
  in a process-safe way and change verbosity at runtime.
* Fixed encoding in name variants dictionaries.
* Fixed local vs global imports.
* Better recompute button on APPP page.
* Upgrade scripts.
* Rabbit names matching functions.
* Provides fix for correct au

modules/miscutil/lib/upgrades/invenio_2014_01_23_bibauthorid_rabbit_matchable_name_column.py
@@ -1,0 +1,72 @@
+# -*- coding: utf-8 -*-
+##
+## This file is part of Invenio.
+## Copyright (C) 2013 CERN.
+##
+## Invenio is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## Invenio is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+import warnings
+from invenio.dbquery import run_sql
+from invenio.bibauthorid_logutils import Logger
+from invenio.bibauthorid_dbinterface import get_name_by_bibref, create_matchable_name
+from invenio.bibauthorid_rabbit import rabbit
+
+depends_on = ['invenio_2013_11_28_bibauthorid_search_engine_column_changes']
+
+def info():
+    return "Updates the columns of aidPERSONIDPAPERS, adds the rabbit matchable name, assigns default value to columns."
+
+def do_upgrade():
+
+    logger = Logger("Rabbit m_name upgrade script")
+
+    warnings.filterwarnings('ignore')
+    run_sql("alter table aidPERSONIDPAPERS add `m_name` VARCHAR(256) not null after name")
+
+    run_sql("alter table aidPERSONIDPAPERS add INDEX `m_name-b` (`m_name`)")
+
+    present_bibrefs = set(run_sql("select bibref_table, bibref_value from aidPERSONIDPAPERS"))
+
+    total_updates = len(present_bibrefs)
+
+
+    records_for_rabbit = set()
+    for i,bibref in enumerate(present_bibrefs):
+        logger.update_status(float(i)/total_updates, '%s out of %s (%s)' % (str(i), str(total_updates), str(bibref)))
+        try:
+            name = get_name_by_bibref(bibref)
+        except AssertionError, error:
+            if "A bibref must have exactly one name" in error.message:
+                records_for_rabbit.add(bibref[1])
+            else:
+                raise error
+        else:
+            m_name = create_matchable_name(name)
+            run_sql("update aidPERSONIDPAPERS set name=%s, m_name=%s where bibref_table=%s "
+                    "and bibref_value=%s ", (name, m_name, bibref[0], bibref[1]))
+    if records_for_rabbit:
+        rabbit(records_for_rabbit)
+    logger.update_status(1., 'Finished')
+
+    run_sql("alter table aidPERSONIDDATA modify  data varchar(256) not null default '' ")
+
+def estimate():
+    """
+    Let's assume 2ms per sql query in a standard production environment, with some safety margin.
+    """
+    n = run_sql("select count(*) from aidPERSONIDPAPERS")[0][0]
+    queries = n*2
+    return 0.002 * queries
+