8e3cb25f0bb4da209a9507f15640900a134675c9
Artem Tsikiridis <artem.tsikiridis@cern.ch>
2014-07-21 10:56:30 +0200

    BAI: Fix forf most relevant name bug.

--- modules/bibauthorid/lib/bibauthorid_name_utils.py
+++ modules/bibauthorid/lib/bibauthorid_name_utils.py
@@ -964,18 +964,20 @@ def most_relevant_name(name_variants):
     '''
     if not name_variants:
         return None
-    name_parts_list = []
+    name_parts_list = list()
 
     for name in name_variants:
-        name_parts_list.append(create_normalized_name(split_name_parts(name)))
-    sorted_by_relevance_name_list = sorted(
-        sorted(name_parts_list,
-               key=lambda k: len(k[1]),
-               reverse=True),
-        key=lambda k: len(k[2]),
-        reverse=True)
+        name_parts_list.append(split_name_parts(name))
 
-    return sorted_by_relevance_name_list[0]
+    sorted_by_initials = sorted(name_parts_list,
+                                key=lambda k: len(k[1]),
+                                reverse=True)
+                                 
+    sorted_by_relevance_name_list = sorted(sorted_by_initials,
+                                           key=lambda k: len(k[2]),
+                                           reverse=True)
+
+    return create_normalized_name(sorted_by_relevance_name_list[0])
 
 GLOBAL_gendernames = _load_gender_firstnames_dict()
 GLOBAL_name_variations = _load_firstname_variations()
