85d8f5e25fae3a20c6763773b9d95f1cd38b9061
Samuele Carli <samuele.carli@cern.ch>
2014-08-12 00:17:38 +0200

    BAI: fix typo

--- modules/bibauthorid/lib/bibauthorid_hoover.py
+++ modules/bibauthorid/lib/bibauthorid_hoover.py
@@ -95,7 +95,7 @@ class ConflictingIdsFromUnreliableSourceException(ConflictingIdsException):
     def get_message_subject(self):
         return "[Hoover] Conflicting identifiers unverified data"
 
-    def get_massega_body(self):
+    def get_message_body(self):
         msg = ["Found conflicting %s identifiers (%s) on profile: " % (self.identifier_type, ','.join(self.ids_list))]
         msg.append("http://inspirehep.net/author/profile/%s" % get_canonical_name_of_author(self.pid) )
         return '\n'.join(msg)
